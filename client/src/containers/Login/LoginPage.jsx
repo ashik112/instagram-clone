5ae87b1a75f6625387547d9d02c3a1b3582a264e
Ashekur Rahman <md.ashekur@gmail.com>
2020-07-22 20:47:38 +0600

    login view

--- client/src/containers/Login/LoginPage.jsx
+++ client/src/containers/Login/LoginPage.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+} from 'reactstrap';
+import { connect } from 'react-redux';
+import logo from '../../assets/logo.png';
+import authActions from '../../redux/reducers/Authentication/authActions';
+import LoginForm from './components/LoginForm';
 import './LoginPage.scss';
 
-const LoginPage = () => (
-    <div>
-        LoginPage
-    </div>
-);
+class LoginPage extends Component {
+  componentDidMount() {
+    const { stopLoading } = this.props;
+    stopLoading();
+  }
 
-export default LoginPage;
+  render() {
+    const { authReducer, onLogIn } = this.props;
+    const { loading } = authReducer;
+    return (
+      <Container fluid className="text-center Login-container" style={{ height: '100vh' }}>
+        {/*<div className="background-image" />*/}
+        <Row style={{ blur: '0 !important' }} className="h-100 justify-content-center align-items-center content">
+          <Col md={4}>
+            <Card className="Login-card" border="warning">
+              <CardBody>
+                <div className="text-center mb-5">
+                  <img className="img-fluid" src={logo} alt="logo" />
+                </div>
+                <LoginForm loading={loading} onLogIn={onLogIn} />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { authReducer } = state;
+  return {
+    authReducer,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  onLogIn: (credentials) => dispatch(
+    authActions.login(
+      credentials,
+    ),
+  ),
+  stopLoading: () => dispatch(authActions.stopLoading()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
---
+++ client/src/containers/Login/components/LoginForm.jsx
@@ -0,0 +1,98 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
+import React, {useState} from 'react';
+import {
+  Button, Form, InputGroup, Spinner, FormGroup, Input, InputGroupAddon,
+} from 'reactstrap';
+import { Field, Formik, Form as ForkmikForm } from 'formik';
+
+const LoginForm = ({ loading, onLogIn }) => {
+  const [isPasswordVisible, setPasswordVisibility] = useState(false);
+  return (
+    <Formik
+      initialValues={{
+        username: '',
+        password: '',
+      }}
+      onSubmit={(values) => {
+        onLogIn({ ...values });
+      }}
+    >
+      {({ handleSubmit, values }) => (
+        <Form as={ForkmikForm}>
+          <Field
+            name="username"
+          >
+            {({ field }) => (
+              <FormGroup>
+                <InputGroup className="mb-2">
+                  <Input
+                    bsSize="sm"
+                    name="username"
+                    id="username"
+                    type="text"
+                    placeholder="username, or email"
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
+                </InputGroup>
+              </FormGroup>
+            )}
+          </Field>
+          <Field
+            name="password"
+          >
+            {({ field }) => (
+              <FormGroup>
+                <InputGroup className="mb-2">
+                  <Input
+                    bsSize="sm"
+                    name="password"
+                    id="password"
+                    autoComplete="on"
+                    type={isPasswordVisible ? 'text' : 'password'}
+                    placeholder="password"
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
+                  {
+                    values.password && (
+                      <InputGroupAddon addonType="append">
+                        { !isPasswordVisible && <Button onClick={() => setPasswordVisibility(true)} size="sm" type="button" color="secondary">Show</Button> }
+                        { isPasswordVisible && <Button onClick={() => setPasswordVisibility(false)} size="sm" type="button" color="secondary">Hide</Button> }
+                      </InputGroupAddon>
+                    )
+                  }
+                </InputGroup>
+              </FormGroup>
+            )}
+          </Field>
+          <Button
+            color="primary"
+            size="sm"
+            block
+            className="text-light"
+            type="submit"
+            disabled={loading || (!values.username || !values.password)}
+            onClick={handleSubmit}
+          >
+            {
+              loading && (
+                <Spinner
+                  as="span"
+                  animation="border"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                />
+              )
+            }
+            &nbsp;&nbsp;Log In
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default LoginForm;
