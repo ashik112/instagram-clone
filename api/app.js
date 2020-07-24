77c5156d2f941338b5267a1cb03135f67adcf6a1
Ashekur Rahman <md.ashekur@gmail.com>
2020-07-24 07:24:56 +0600

    disable force sync

--- api/app.js
+++ api/app.js
@@ -52,10 +52,10 @@ app.use((err, req, res, next) => {
 
 /// db.sequelize.sync(); // enable in production mode
 // create all the defined tables in the specified database. ! { force: true } will drop tables
-db.sequelize.sync({ force: true, alter: true, drop: true }).then(() => {
-  console.log('Re-Sync Database.');
+db.sequelize.sync({ force: false, alter: true, drop: false }).then(() => {
+  console.log('re-sync database.');
 }).catch((error) => {
-  console.log('ERROR: ', error);
+  console.log('error: ', error);
 });
 
 module.exports = app;
