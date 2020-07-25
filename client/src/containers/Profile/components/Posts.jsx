dc991e5bfce02256f00d8a4c9cbf2a38f5b4ce76
Ashekur Rahman <md.ashekur@gmail.com>
2020-07-25 10:25:06 +0600

    Fix modal responsiveness

--- client/src/containers/Profile/components/Posts.jsx
+++ client/src/containers/Profile/components/Posts.jsx
@@ -1,17 +1,14 @@
 // eslint-disable-next-line max-len
-/* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role,react/jsx-one-expression-per-line */
+/* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role,react/jsx-one-expression-per-line,jsx-a11y/anchor-is-valid,jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
 import React, { useEffect, useState } from 'react';
-import { MdGridOn } from 'react-icons/md';
 import {
   Container, Row, Col, Modal,
 } from 'reactstrap';
 import './Posts.scss';
 import { BsFillHeartFill, BsFillChatFill } from 'react-icons/bs';
-import Card from 'reactstrap/es/Card';
-import CardBody from 'reactstrap/es/CardBody';
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft, MdGridOn } from 'react-icons/md';
 import axios from '../../../utils/axios';
 import { apiUrl } from '../../../constants';
-import UserListItem from '../../../shared/components/User/UserListItem';
 
 // eslint-disable-next-line react/prop-types
 const Posts = ({ user }) => {
@@ -59,37 +56,56 @@ const Posts = ({ user }) => {
       {
         selectedPost && (
           <Modal
+            className="Compact"
             isOpen
             external={(
-              <button
+              <a
                 type="button"
                 className="close text-light"
                 style={{ position: 'absolute', top: '15px', right: '15px' }}
                 onClick={() => setSelectedPost(null)}
               >
                 &times;
-              </button>
+              </a>
             )}
           >
-            <div className="d-flex flex-row justify-content-between">
-              <img
-                className="Posts-modal-img flex-column"
-                src={`${apiUrl}/ftp/uploads/${selectedPost.photo}`}
-                alt="N/A"
-              />
-              <div className="flex-column" style={{ minWidth: 300, backgroundColor: '#FFFFFF' }}>
-                <div className="d-flex border-bottom justify-content-start align-items-center p-3">
+            <div>
+              <Row className="m-0 p-0">
+                <Col className="p-0 m-0" md={9}>
                   <img
-                    className="img-avatar"
-                    src={`${apiUrl}/ftp/uploads/${selectedPost.user.avatar}`}
+                    className="Posts-modal-img"
+                    src={`${apiUrl}/ftp/uploads/${selectedPost.photo}`}
                     alt="N/A"
                   />
-                  <div>
-                    <h5 className="pl-2">{selectedPost.user.name}</h5>
+                </Col>
+                <Col className="p-0 m-0" md={3}>
+                  <div className="d-flex border-bottom justify-content-start align-items-center p-3">
+                    <img
+                      className="img-avatar"
+                      src={`${apiUrl}/ftp/uploads/${selectedPost.user.avatar}`}
+                      alt="N/A"
+                    />
+                    <div>
+                      <h5 className="pl-2">{selectedPost.user.name}</h5>
+                    </div>
                   </div>
-                </div>
-              </div>
+                </Col>
+              </Row>
             </div>
+            <a
+              type="button"
+              className="close text-light"
+              style={{ position: 'absolute', top: '48%', right: '-30px' }}
+            >
+              <MdKeyboardArrowRight />
+            </a>
+            <a
+              type="button"
+              className="close text-light"
+              style={{ position: 'absolute', top: '48%', left: '-30px' }}
+            >
+              <MdKeyboardArrowLeft />
+            </a>
           </Modal>
         )
       }
