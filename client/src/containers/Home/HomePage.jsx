a8779a2220ce7a02b78569c3df77905dad7a1c8b
Ashekur Rahman <md.ashekur@gmail.com>
2020-07-25 06:24:32 +0600

    Add user list, user profile

--- client/src/containers/Home/HomePage.jsx
+++ client/src/containers/Home/HomePage.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import { Row, Col } from 'reactstrap';
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import axios from '../../utils/axios';
+import { apiUrl } from '../../constants';
+import UserListItem from '../../shared/components/User/UserListItem';
 
-const HomePage = () => (
-  <>
-    <Row>
-      <Col>
-        Home Page
-      </Col>
-    </Row>
-  </>
-);
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`${apiUrl}/users`).then((res) => {
+      this.setState({
+        users: [...res.data],
+      });
+    }).catch((e) => e.handleGlobally(e));
+  }
+
+  render() {
+    const { users } = this.state;
+    return (
+      <Container fluid>
+        <h5 className="text-grey">Users</h5>
+        {
+          users && users.map((user) => (
+            <div key={user.id}>
+              <UserListItem user={user} />
+            </div>
+          ))
+        }
+      </Container>
+    );
+  }
+}
 
 export default HomePage;
--- client/src/containers/Profile/ProfilePage.jsx
+++ client/src/containers/Profile/ProfilePage.jsx
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
 import ProfileHeader from './components/ProfileHeader';
 import Posts from './components/Posts';
+import axios from '../../utils/axios';
+import { apiUrl } from '../../constants';
+import NoMatch from '../../shared/components/NoMatch';
 
-const ProfilePage = (props) => {
-  console.log(props);
-  return (
-    <>
-      <Row>
-        <Col>
-          <ProfileHeader />
-        </Col>
-      </Row>
-      <Row className="pt-2">
-        <Col>
-          <Posts />
-        </Col>
-      </Row>
-    </>
-  );
-};
+class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true,
+    }, () => {
+      // eslint-disable-next-line react/prop-types
+      const { match: { params: { username } } } = this.props;
+      axios.get(`${apiUrl}/users/username/${username}`).then((res) => {
+        this.setState({
+          user: res.data,
+          loading: false,
+        });
+      }).catch((e) => {
+        console.log(e);
+        this.setState({
+          loading: false,
+        });
+      });
+    });
+  }
+
+  render() {
+    const { user, loading } = this.state;
+    return (
+      <>
+        {
+          (user && (
+            <>
+              <Row>
+                <Col>
+                  <ProfileHeader user={user} />
+                </Col>
+              </Row>
+              <Row className="pt-2">
+                <Col>
+                  <Posts user={user} />
+                </Col>
+              </Row>
+            </>
+          )) || (!loading && <NoMatch />)
+        }
+      </>
+    );
+  }
+}
 
 export default ProfilePage;
