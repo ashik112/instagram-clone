483c92f41aac8b0edbea87f3f6269c9266a2bf89
Ashekur Rahman <md.ashekur@gmail.com>
2020-07-27 14:07:22 +0600

    Fix modal for mobile devices

--- client/src/containers/Gallery/Modal/PostModal.jsx
+++ client/src/containers/Gallery/Modal/PostModal.jsx
@@ -1,9 +1,12 @@
 // eslint-disable-next-line max-len
 /* eslint-disable react/prop-types,react/jsx-one-expression-per-line,jsx-a11y/anchor-is-valid,jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
 import React from 'react';
-import { Col, Modal, Row } from 'reactstrap';
+import {
+  Col, Modal, ModalHeader, Row,
+} from 'reactstrap';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 import { BsHeart, BsChat } from 'react-icons/bs';
+import { useMediaQuery } from 'react-responsive';
 import CommentsHeader from './CommentsHeader';
 import CommentsItem from './CommentsItem';
 import './PostModal.scss';
@@ -12,95 +15,121 @@ import { apiRoutes } from '../../../routing/apiRoutes';
 
 const PostModal = ({
   post, postsLength, onClose, onClickNext, postIndex, onClickPrevious,
-}) => (
-  <Modal
-    toggle={onClose}
-    className="Compact"
-    isOpen
-    external={(
-      <a
-        type="button"
-        className="close text-light"
-        style={{ position: 'absolute', top: '15px', right: '15px' }}
-        onClick={onClose}
-      >
-        &times;
-      </a>
-    )}
-  >
-    <div>
-      <Row className="m-0 p-0">
-        <Col className="p-0 m-0" md={9}>
-          <div className="post-modal-img__container d-flex justify-content-center align-items-center">
-            <img
-              className="post-modal-img align-self-center"
-              src={apiRoutes.imageSrc(post.photo)}
-              alt=""
-            />
-          </div>
-        </Col>
-        <Col className="p-0 m-0 overflow-hidden" md={3}>
-          <div className="sidebar">
-            <div className="sidebar-header">
-              <div className="d-flex border-bottom justify-content-start align-items-center p-3">
-                <img
-                  className="img-avatar"
-                  src={post.user.avatar ? apiRoutes.imageSrc(post.user.avatar) : imgAlt}
-                  alt=""
-                />
-                <div className="pl-2">
-                  <span><b>{post.user.username}</b></span>
+}) => {
+  const isMobile = useMediaQuery({
+    query: '(max-width: 767px)',
+  });
+
+  return (
+    <Modal
+      toggle={onClose}
+      className="Compact"
+      isOpen
+      external={(
+        <a
+          type="button"
+          className="close text-light"
+          style={{ position: 'absolute', top: '15px', right: '15px' }}
+          onClick={onClose}
+        >
+          &times;
+        </a>
+      )}
+    >
+      {
+        isMobile && (
+          <ModalHeader toggle={onClose}>
+            <div className="d-flex justify-content-start align-items-center">
+              <img
+                className="img-avatar"
+                src={post.user.avatar ? apiRoutes.imageSrc(post.user.avatar) : imgAlt}
+                alt=""
+              />
+              <div className="pl-2">
+                <span><b>{post.user.username}</b></span>
+              </div>
+            </div>
+          </ModalHeader>
+        )
+      }
+      <div>
+        <Row className="m-0 p-0">
+          <Col className="p-0 m-0" md={9}>
+            <div className="post-modal-img__container d-flex justify-content-center align-items-center">
+              <img
+                className="post-modal-img align-self-center"
+                src={apiRoutes.imageSrc(post.photo)}
+                alt=""
+              />
+            </div>
+          </Col>
+          <Col className="p-0 m-0 overflow-hidden" md={3}>
+            <div className="sidebar">
+              {
+                !isMobile && (
+                  <div className="sidebar-header">
+                    <div className="d-flex border-bottom justify-content-start align-items-center p-3">
+                      <img
+                        className="img-avatar"
+                        src={post.user.avatar ? apiRoutes.imageSrc(post.user.avatar) : imgAlt}
+                        alt=""
+                      />
+                      <div className="pl-2">
+                        <span><b>{post.user.username}</b></span>
+                      </div>
+                    </div>
+                  </div>
+                )
+              }
+              <div className="sidebar-body">
+                <div className="comment-list">
+                  <CommentsHeader post={post} />
+                  {
+                    post.comments && post.comments.map((comment) => (
+                      <div key={comment.id} className="pb-3">
+                        <CommentsItem comment={comment} />x
+                      </div>
+                    ))
+                  }
                 </div>
               </div>
-            </div>
-            <div className="sidebar-body">
-              <div className="comment-list">
-                <CommentsHeader post={post} />
-                {
-                  post.comments && post.comments.map((comment) => (
-                    <div key={comment.id} className="pb-3">
-                      <CommentsItem comment={comment} />
-                    </div>
-                  ))
-                }
+              <div className="sidebar-footer border-top align-middle">
+                <span>
+                  <b><BsHeart /> {post.likes.length}</b> {post.likes.length > 1 ? 'likes' : 'like' }
+                &nbsp;&nbsp;&nbsp;
+                  <b><BsChat /> {post.comments.length}</b> {post.comments.length > 1 ? 'comments' : 'comment' }
+                </span>
               </div>
             </div>
-            <div className="sidebar-footer border-top align-middle">
-              <span>
-                <b><BsHeart /> {post.likes.length}</b> {post.likes.length > 1 ? 'likes' : 'like' }
-                &nbsp;&nbsp;&nbsp;
-                <b><BsChat /> {post.comments.length}</b> {post.comments.length > 1 ? 'comments' : 'comment' }
-              </span>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </div>
-    {
-      postIndex < postsLength - 1 && (
-        <a
-          onClick={onClickNext}
-          type="button"
-          className="close text-light"
-          style={{ position: 'absolute', top: '48%', right: '-50px' }}
-        >
-          <MdKeyboardArrowRight size={48} />
-        </a>
-      )
-    }
-    {
-      postIndex > 0 && (
-        <a
-          onClick={onClickPrevious}
-          type="button"
-          className="close text-light"
-          style={{ position: 'absolute', top: '48%', left: '-50px' }}
-        >
-          <MdKeyboardArrowLeft size={48} />
-        </a>
-      )
-    }
-  </Modal>
-);
+          </Col>
+        </Row>
+      </div>
+      {
+        postIndex < postsLength - 1 && (
+          <a
+            onClick={onClickNext}
+            type="button"
+            className="close text-light"
+            style={{ position: 'absolute', top: '48%', right: '-50px' }}
+          >
+            <MdKeyboardArrowRight size={48} />
+          </a>
+        )
+      }
+      {
+        postIndex > 0 && (
+          <a
+            onClick={onClickPrevious}
+            type="button"
+            className="close text-light"
+            style={{ position: 'absolute', top: '48%', left: '-50px' }}
+          >
+            <MdKeyboardArrowLeft size={48} />
+          </a>
+        )
+      }
+    </Modal>
+  );
+};
 
 export default PostModal;
